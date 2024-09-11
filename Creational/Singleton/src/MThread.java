8878e61f8adf937e37918f4add2ca9ce504cf29e
Tuấn <147260307+Yume0308@users.noreply.github.com>
2024-09-11 15:36:36 +0700

    Singleton: Add: MThread.java

---
+++ Creational/Singleton/src/MThread.java
@@ -0,0 +1,16 @@
+public class MThread {
+    static class ThreadFoo implements Runnable {
+        @Override
+        public void run() {
+            Singleton singleton = Singleton.getInstance("FOO");
+            System.out.println(singleton.value);
+        }
+    }
+    static class ThreadBar implements Runnable {
+        @Override
+        public void run() {
+            Singleton singleton = Singleton.getInstance("BAR");
+            System.out.println(singleton.value);
+        }
+    }
+}
